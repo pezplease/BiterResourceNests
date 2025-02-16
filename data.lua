94b6e76e5793b96d8a00d5e65e345bd66f515694
PezPlease <tatmanbernard@gmail.com>
2025-02-16 01:48:58 -0600

    Refactored biter spawner setup to use a base generic spawner, and to use unique nest behavior per resource type.

--- data.lua
+++ data.lua
@@ -1,13 +1,3 @@
-local resource_types = {
-  "iron-ore",
-  "copper-ore",
-  "coal",
-  "stone",
-  "uranium-ore",
-  "crude-oil"
-}
-
-
 --[[
 local fireArmor = table.deepcopy(data.raw["armor"]["heavy-armor"]) -- copy the table that defines the heavy armor item into the fireArmor variable
 
@@ -69,100 +59,278 @@ local resource_colors = {
   ["uranium-ore"] = { r = 0.5, g = 0.5, b = 0.5, a = 0.6 },
   ["crude-oil"] = { r = 0.89, g = 0.349, b = 0.588, a = 0.6 }
 }
+local resource_types = {
+  "iron-ore",
+  "copper-ore",
+  "coal",
+  "stone",
+  "uranium-ore",
+  "crude-oil"
+}
+local default_nest = {
+  "generic-nest"
+}
+--remote.add_interface("resource_list", {
+--  get_resource_list = function()
+--    return resource_types
+--  end
+--})
 
+
+function create_resistance_table(physdec, physperc, expdec, expperc, aciddec, acidperc, firedec, fireperc, laserdec,
+  laserperc, elecdec, elecperc, poisdec, poisperc, impdec, impperc)
+local resistance_list = {
+{
+type = "physical",
+decrease = physdec,
+percent = physperc
+},
+{
+type = "explosion",
+decrease = expdec,
+percent = expperc
+},
+{
+type = "acid",
+decrease = aciddec,
+percent = acidperc
+},
+{
+type = "fire",
+decrease = firedec,
+percent = fireperc
+},
+{
+type = "laser",
+decrease = laserdec,
+percent = laserperc
+},
+{
+type = "electric",
+decrease = elecdec,
+percent = elecperc
+},
+{
+type = "poison",
+decrease = poisdec,
+percent = poisperc
+},
+{
+type = "impact",
+decrease = impdec,
+percent = impperc
+}
+}
+return resistance_list
+end
+
+
+spitter_units = {
+  {
+    "small-biter",
+    {
+      {
+        0,
+        0.3
+      },
+      {
+        0.35,
+        0
+      }
+    }
+  },
+  {
+    "small-spitter",
+    {
+      {
+        0,
+        0.3
+      },
+      {
+        0.5,
+        0.3
+      },
+      {
+        0.6,
+        0
+      }
+    }
+  },
+  {
+    "medium-spitter",
+    {
+      {
+        0.2,
+        0
+      },
+      {
+        0.7,
+        0.3
+      },
+      {
+        0.9,
+        0.1
+      }
+    }
+  },
+  {
+    "big-spitter",
+    {
+      {
+        0.2,
+        0
+      },
+      {
+        1,
+        0.4
+      }
+    }
+  },
+  {
+    "big-biter",
+    {
+      {
+        0.3,
+        0
+      },
+      {
+        1,
+        0.4
+      }
+    }
+  },
+  {
+    "behemoth-spitter",
+    {
+      {
+        0.75,
+        0
+      },
+      {
+        1,
+        0.1
+      }
+    }
+  }
+}
 
 
 --default active values
-local default_nest_health = 3000
-local default_active_nest_cooldown = { 25, 35 }
+local default_nest_health = 7500
+local default_active_nest_cooldown = { 5, 10 }
 local default_active_max_count_of_owned_units = 100
+local default_resistances = create_resistance_table(10, 80, 0, 80, 0, 80, 0, 80, 0, 80, 0, 80, 0, 80, 0, 80)
+
 --default inactive values
 local default_inactive_nest_cooldown = { 999999, 999999 }
 local default_inactive_max_count_of_owned_units = 0
 local default_inactive_max_count_defensive_units = 0
 
-local resourcespawners = {}
-for _, resource_name in pairs(resource_types) do
-  local inactive_spawner = table.deepcopy(data.raw["unit-spawner"]["biter-spawner"])
-  inactive_spawner.name = "inactive-biter-spawner-" .. resource_name
-  inactive_spawner.max_health = default_nest_health
-  inactive_spawner.max_count_of_owned_units = default_inactive_max_count_of_owned_units -- Prevent spawning
-  inactive_spawner.spawning_cooldown = default_inactive_nest_cooldown
-  inactive_spawner.max_count_of_owned_defensive_units = default_inactive_max_count_defensive_units
 
-  local active_spawner = table.deepcopy(data.raw["unit-spawner"]["biter-spawner"])
-  active_spawner.name = "active-biter-spawner-" .. resource_name
-  active_spawner.max_health = default_nest_health
-  active_spawner.spawning_cooldown = default_active_nest_cooldown
-  active_spawner.max_count_of_owned_units = default_active_max_count_of_owned_units
-  --override default resource settings
-  if resource_name == "crude-oil" then
-    inactive_spawner.max_health = 1000
-    active_spawner.max_health = 1000
-    active_spawner.default_active_nest_cooldown = { 50, 60 }
-    active_spawner.max_count_of_owned_units = 20
-  end
+local generic_spawner = table.deepcopy(data.raw["unit-spawner"]["biter-spawner"])
+generic_spawner.name = "base-resource-spawner"
+data.raw["unit-spawner"]["base-resource-spawner"] = generic_spawner
+data:extend({ generic_spawner })
 
-  -- Set the tint for the spawner
-  for res_name, resource_colors in pairs(resource_colors) do
-    if resource_name == res_name then
-      inactive_spawner.max_health = 6000
-      active_spawner.icons = {
-        {
-          icon = active_spawner.icon,
-          icon_size = active_spawner.icon_size,
-          tint = resource_colors
-        },
-      }
-      inactive_spawner.icons = {
-        {
-          icon = inactive_spawner.icon,
-          icon_size = inactive_spawner.icon_size,
-          tint = resource_colors
-        },
-      }
+function setup_resource_nests(resource_list)
+  local resourcespawners = {}
+  for _, resource_name in pairs(resource_list) do
+    local inactive_spawner = table.deepcopy(generic_spawner)
+    inactive_spawner.name = "inactive-biter-spawner-" .. resource_name
+    inactive_spawner.max_health = default_nest_health
+    inactive_spawner.max_count_of_owned_units = default_inactive_max_count_of_owned_units -- Prevent spawning
+    inactive_spawner.spawning_cooldown = default_inactive_nest_cooldown
+    inactive_spawner.max_count_of_owned_defensive_units = default_inactive_max_count_defensive_units
+    inactive_spawner.resistances = default_resistances
 
 
-      if inactive_spawner.graphics_set and inactive_spawner.graphics_set.animations then
-        for _, animation in pairs(inactive_spawner.graphics_set.animations) do
+    local active_spawner = table.deepcopy(generic_spawner)
+    active_spawner.name = "active-biter-spawner-" .. resource_name
+    active_spawner.max_health = default_nest_health
+    active_spawner.spawning_cooldown = default_active_nest_cooldown
+    active_spawner.max_count_of_owned_units = default_active_max_count_of_owned_units
+    active_spawner.resistances = default_resistances
+    --override default resource settings
+    if resource_name == "crude-oil" then
+      inactive_spawner.max_health = 1000
+      active_spawner.max_health = 1000
+      active_spawner.default_active_nest_cooldown = { 50, 60 }
+      active_spawner.max_count_of_owned_units = 20
+    elseif resource_name == "copper-ore" then
+      inactive_spawner.result_units = spitter_units
+      active_spawner.result_units = spitter_units
+    end
+    -- Set the tint for the spawner
+    for res_name, resource_colors in pairs(resource_colors) do
+      if resource_name == res_name then
+        active_spawner.icons = {
+          {
+            icon = active_spawner.icon,
+            icon_size = active_spawner.icon_size,
+            tint = resource_colors
+          },
+        }
+        inactive_spawner.icons = {
+          {
+            icon = inactive_spawner.icon,
+            icon_size = inactive_spawner.icon_size,
+            tint = resource_colors
+          },
+        }
+        if active_spawner.graphics_set and active_spawner.graphics_set.animations then
+          for _, animation in pairs(active_spawner.graphics_set.animations) do
             if animation.layers then
-                for _, layer in pairs(animation.layers) do
-                    if layer then
-                        layer.tint = resource_colors
-                        if layer.hr_version then
-                            layer.hr_version.tint = resource_colors
-                        end
-                    end
+              for _, layer in pairs(animation.layers) do
+                if layer then
+                  layer.tint = resource_colors
+                  if layer.hr_version then
+                    layer.hr_version.tint = resource_colors
+                  end
                 end
+              end
             else
-                animation.tint = resource_colors
-                if animation.hr_version then
-                    animation.hr_version.tint = resource_colors
-                end
+              animation.tint = resource_colors
+              if animation.hr_version then
+                animation.hr_version.tint = resource_colors
+              end
             end
+          end
+        else
+          log("Warning: inactive_spawner.graphics_set.animations is nil. Unable to apply tint.") -- Debug message
         end
-    else
-        log("Warning: inactive_spawner.graphics_set.animations is nil. Unable to apply tint.") -- Debug message
+
+        if inactive_spawner.graphics_set and inactive_spawner.graphics_set.animations then
+          for _, animation in pairs(inactive_spawner.graphics_set.animations) do
+            if animation.layers then
+              for _, layer in pairs(animation.layers) do
+                if layer then
+                  layer.tint = resource_colors
+                  if layer.hr_version then
+                    layer.hr_version.tint = resource_colors
+                  end
+                end
+              end
+            else
+              animation.tint = resource_colors
+              if animation.hr_version then
+                animation.hr_version.tint = resource_colors
+              end
+            end
+          end
+        else
+          log("Warning: inactive_spawner.graphics_set.animations is nil. Unable to apply tint.") -- Debug message
+        end
+      end
     end
-    end
+
+
+    table.insert(resourcespawners, inactive_spawner)
+    table.insert(resourcespawners, active_spawner)
   end
-
-
-  table.insert(resourcespawners, inactive_spawner)
-  table.insert(resourcespawners, active_spawner)
+  data:extend(resourcespawners)
 end
-data:extend(resourcespawners)
 
---[[
+setup_resource_nests(resource_types)
+setup_resource_nests(default_nest)
 
-      --active_spawner.animations[1].tint_as_overlay = true
-      active_spawner.tint = resource_colors
-      active_spawner.apply_runtime_tint = true
-      active_spawner.tint_as_overlay = true
 
-            inactive_spawner.tint = resource_colors
-      inactive_spawner.apply_runtime_tint = true
-      inactive_spawner.tint_as_overlay = true
-      --inactive_spawner.animations[1].tint_as_overlay = true
-
-]]
