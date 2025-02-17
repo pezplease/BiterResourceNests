0c420cd0416a30551aec3dc04802a42b6e32adfe
PezPlease <tatmanbernard@gmail.com>
2025-02-17 00:26:26 -0600

    removed tutorial code

--- data.lua
+++ data.lua
@@ -1,50 +1,4 @@
---[[
-local fireArmor = table.deepcopy(data.raw["armor"]["heavy-armor"]) -- copy the table that defines the heavy armor item into the fireArmor variable
 
-fireArmor.name = "fire-armor"
-fireArmor.icons = {
-  {
-    icon = fireArmor.icon,
-    icon_size = fireArmor.icon_size,
-    tint = {r=1,g=0,b=0,a=0.3}
-  },
-}
---]]
-
---biter nest variations
-local custom_spawner = table.deepcopy(data.raw["unit-spawner"]["biter-spawner"])
-custom_spawner.name = "self-damaging-spawner"
-custom_spawner.icon = "__base__/graphics/icons/biter-spawner.png"
-custom_spawner.max_health = 3000
-custom_spawner.healing_per_tick = .5
-custom_spawner.max_count_of_owned_units = 0
-custom_spawner.max_count_of_owned_defensive_units = 0
-custom_spawner.max_friends_around_to_spawn = 0
-custom_spawner.max_defensive_friends_around_to_spawn = 0
-custom_spawner.spawning_cooldown = { 25, 35 }
-custom_spawner.resistances = {
-  {
-    type = "physical",
-    decrease = 10,
-    percent = 99
-  },
-  {
-    type = "explosion",
-    decrease = 10,
-    percent = 99
-  },
-  {
-    type = "acid",
-    decrease = 5,
-    percent = 99
-  },
-  {
-    type = "fire",
-    decrease = 0,
-    percent = 99
-  }
-}
-data:extend({ custom_spawner })
 local inactive_nest = table.deepcopy(data.raw["unit-spawner"]["biter-spawner"])
 inactive_nest.name = "inactive-spawner"
 inactive_nest.max_count_of_owned_units = 0
@@ -52,12 +6,12 @@ data:extend({ inactive_nest })
 
 --resource nest colors
 local resource_colors = {
-  ["iron-ore"] = { r = 0.15, g = 0.15, b = 0.9, a = 0.6 },
-  ["copper-ore"] = { r = 0.803, g = 0.388, b = 0.215, a = 0.6 },
-  ["coal"] = { r = 0.2, g = 0.2, b = 0.2, a = 0.6 },
-  ["stone"] = { r = 0.5, g = 0.5, b = 0.5, a = 0.6 },
-  ["uranium-ore"] = { r = 0.5, g = 0.5, b = 0.5, a = 0.6 },
-  ["crude-oil"] = { r = 0.89, g = 0.349, b = 0.588, a = 0.6 }
+  ["iron-ore"] = { r = 0.15, g = 0.15, b = 0.7, a = 0.8 },
+  ["copper-ore"] = { r = 0.803, g = 0.388, b = 0.215, a = 0.8 },
+  ["coal"] = { r = 0.2, g = 0.2, b = 0.2, a = 0.8 },
+  ["stone"] = { r = 0.5, g = 0.5, b = 0.5, a = 0.8 },
+  ["uranium-ore"] = { r = 0.23, g = 0.91, b = 0.2, a = 0.8 },
+  ["crude-oil"] = { r = 0.89, g = 0.349, b = 0.588, a = 0.8 }
 }
 local resource_types = {
   "iron-ore",
