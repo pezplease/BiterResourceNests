ba7e009c4daea5562992d5550e38406eebeed75a
PezPlease <tatmanbernard@gmail.com>
2025-02-24 23:00:44 -0600

    moved data from the resource biters file to here. also created resource overrides for biter and spawner stats.

--- prototypes/biter-data.lua
+++ prototypes/biter-data.lua
@@ -1,9 +1,165 @@
+function specilized_biter_results(resource_type)
+    biter_units = {
+        {
+            resource_type .. "-" .. "small-biter",
+            {
+                {
+                    0,
+                    0.3
+                },
+                {
+                    0.6,
+                    0
+                }
+            }
+        },
+        {
+            resource_type .. "-" .. "medium-biter",
+            {
+                {
+                    0,
+                    .1
+                },
+                {
+                    0.6,
+                    0.3
+                },
+                {
+                    0.7,
+                    0.1
+                }
+            }
+        },
+        {
+            resource_type .. "-" .. "big-biter",
+            {
+                {
+                    0.2,
+                    0
+                },
+                {
+                    1,
+                    0.4
+                }
+            }
+        },
+        {
+            resource_type .. "-" .. "behemoth-biter",
+            {
+                {
+                    0.85,
+                    0
+                },
+                {
+                    1,
+                    0.3
+                }
+            }
+        }
+    }
+    return biter_units
+end
+
+function specilized_spitter_results(resource_type)
+    spitter_units = {
+        {
+            resource_type .. "-" .. "small-biter",
+            {
+                {
+                    0,
+                    0.3
+                },
+                {
+                    0.35,
+                    0
+                }
+            }
+        },
+        {
+            resource_type .. "-" .. "small-spitter",
+            {
+                {
+                    0,
+                    0.3
+                },
+                {
+                    0.35,
+                    0.1
+                },
+                {
+                    0.6,
+                    0
+                }
+            }
+        },
+        {
+            resource_type .. "-" .. "medium-spitter",
+            {
+                {
+                    0.2,
+                    0.2
+                },
+                {
+                    0.7,
+                    0.3
+                },
+                {
+                    0.9,
+                    0.1
+                }
+            }
+        },
+        {
+            resource_type .. "-" .. "big-spitter",
+            {
+                {
+                    0.2,
+                    0.05
+                },
+                {
+                    1,
+                    0.4
+                }
+            }
+        },
+        {
+            resource_type .. "-" .. "big-biter",
+            {
+                {
+                    0.2,
+                    0.01
+                },
+                {
+                    1,
+                    0.4
+                }
+            }
+        },
+        {
+            resource_type .. "-" .. "behemoth-spitter",
+            {
+                {
+                    0.75,
+                    0
+                },
+                {
+                    1,
+                    0.1
+                }
+            }
+        }
+    }
+    return spitter_units
+end
+
+--default list of biters for generic resource
 biter_list = {
     "small-biter",
     "medium-biter",
     "big-biter",
     "behemoth-biter",
 }
+--specialized list of biters and spitters
 spitter_list = {
     "small-biter",
     "medium-biter",
@@ -21,7 +177,7 @@ resource_colors = {
     ["iron-ore"] = { r = 0.45, g = 0.6, b = 0.9, a = 1 },
     ["copper-ore"] = { r = 0.803, g = 0.388, b = 0.215, a = 1 },
     ["coal"] = { r = 0.2, g = 0.2, b = 0.2, a = 1 },
-    ["stone"] = { r = 0.8, g = 0.5, b = 0.1, a = 1 },
+    ["stone"] = { r = 0.95, g = 0.8, b = 0.7, a = 1 },
     ["uranium-ore"] = { r = 0.25, g = 0.91, b = 0.25, a = 1 },
     ["crude-oil"] = { r = 0.89, g = 0.349, b = 0.588, a = 1 }
 }
@@ -35,4 +191,35 @@ resource_types = {
 }
 default_nest = {
     "generic-nest"
+}
+
+resource_units = {
+    ["copper-ore"] = spitter_list
+}
+
+resource_overrides = {
+    ["iron-ore"] = {
+        max_health = 5000,
+        spawning_cooldown = { 2, 3 },
+    },
+    ["copper-ore"] = {
+        max_health = 7800
+        --results_units = specilized_spitter_results,
+    },
+    ["crude-oil"] = {
+        max_health = 1000,
+        spawning_cooldown = { 33, 35 },
+    }
+}
+
+biter_overrides = {
+    ["generic"] = {
+        health_multiplier = 1.5,
+        speed_multiplier = 0.75,
+        damage_multiplier = 1.25,
+    },
+    ["stone"] = {
+        health_multiplier = 3,
+        speed_multiplier = .3
+    }
 }
