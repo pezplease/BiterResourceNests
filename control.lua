ac3c708ae39f24bbfdc39a5b6de74f180adfa01c
PezPlease <tatmanbernard@gmail.com>
2025-03-09 20:47:08 -0500

    moved initilizations to seperate file

--- control.lua
+++ control.lua
@@ -1,42 +1,9 @@
-script.on_init(function()
-  storage.spawned_nests = {}
-  storage.last_biter_kill = 0
-  storage.active_nests = {}
-  --add any resources here that you want to not have a nest spawn the first time it's encountered
-  --higher numbers means more than one patch will be avoided
-  storage.patches_to_remove = {
-    ["iron-ore"] = 1,
-    ["copper-ore"] = 1,
-    ["coal"] = 1,
-    ["stone"] = 1,
-    --["uranium-ore"] = 1,
-    --["crude-oil"] = 1
-  }
-  storage.remove_patches = settings.startup["starting-resource-exemption"].value
-  removenormalnests()
-end)
+require "prototypes.biter-data"
+
+--require "script.resource_nests"
+require "script.initilization"
+
 --todo find all resources automatically.
-local resource_types = {
-  "iron-ore",
-  "copper-ore",
-  "coal",
-  "stone",
-  "uranium-ore",
-  "crude-oil"
-}
-
-function removenormalnests()
-  local surface = game.surfaces["nauvis"]
-
-  if settings.startup["remove-normal-nests"].value then
-    local map_settings = surface.map_gen_settings
-    map_settings.autoplace_controls["enemy-base"] = { frequency = "none", size = "none", richness = "none" }
-    surface.map_gen_settings = map_settings
-    for _, entity in pairs(surface.find_entities_filtered { type = { "unit", "unit-spawner", "turret" } }) do
-      entity.destroy()
-    end
-  end
-end
 
 local function position_key(position)
   -- Helper to create a unique key for a position
