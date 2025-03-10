b2ecb202683a0a7bf2822a5e5e90e7081bb647e2
PezPlease <tatmanbernard@gmail.com>
2025-03-09 20:47:37 -0500

    began work on new data system for biter resources

--- data.lua
+++ data.lua
@@ -1,11 +1,17 @@
-require "prototypes.resource-biters"
 require "prototypes.biter-data"
+require "prototypes.resource-biters"
+
 
 --remote.add_interface("resource_list", {
 --  get_resource_list = function()
 --    return resource_types
 --  end
 --})
+
+--creates the data for the biters and nests
+add_res_list_to_table(resource_list)
+
+
 --setup all biter types for each resource, with a generic fallback nest
 setup_resource_biters(resource_types)
 setup_resource_biters(default_nest)
