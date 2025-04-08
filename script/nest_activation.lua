ed9fdd4d3d58460df0c9cc9277c0d5beb5af4ae9
PezPlease <tatmanbernard@gmail.com>
2025-04-08 01:57:53 -0500

    finished activation and attack logic for nests

--- script/nest_activation.lua
+++ script/nest_activation.lua
@@ -1,92 +1,131 @@
 --require "control"
 require "script.initilization"
---checks if a player mines a resource patch.
-script.on_event(defines.events.on_player_mined_entity, function(event)
-    local entity = event.entity
-    local nest_type = "generic"
-    game.print("Entity mined: " .. entity.name)
-    for _, resource_check_data in pairs(resource_list) do
-      local resource_check = resource_check_data.name
-      if resource_check == entity.name then
-        nest_type = entity.name
-        break
-      end
-    end
-    local nests = entity.surface.find_entities_filtered {
-      name = "inactive-biter-spawner-" .. nest_type,
-      position = entity.position,
-      radius = 62
-    }
-    for _, nest in pairs(nests) do
-      if not nest.valid then return end
-      activate_nest(nest, event.entity.name)
-    end
-  end)
-  function check_nest_activity()
-    if not storage.active_nests then return end
-    for i, nest_info in pairs(storage.active_nests) do
-      if nest_info.entity and (game.tick - nest_info.lastspawn > 600) then
-        deactivate_nest(nest_info.entity)
-        table.remove(storage.active_nests, i)
-      elseif nest_info.entity.valid then --nest_info.entity then
+
+
+
+function check_nest_activity()
+  if not storage.active_nests then return end
+  for i, nest_info in pairs(storage.active_nests) do
+    if nest_info.entity and (game.tick - nest_info.lastspawn > 600) then
+      deactivate_nest(nest_info.entity)
+      table.remove(storage.active_nests, i)
+    elseif nest_info.entity.valid then   --nest_info.entity then
       shoot_nest_projectile(nest_info.entity, nest_info.resource)
     end
-    end
   end
-  
-  function deactivate_nest(nest)
-    if not nest or not nest.valid then return end
-    local inactive_nest_name = "in" .. nest.name
-    local surface = game.surfaces["nauvis"]
-    local inactive_nest = surface.create_entity({
-      name = inactive_nest_name,
-      position = nest.position,
-      force = "enemy"
-    })
-    inactive_nest.health = nest.health
-  
-    nest.destroy()
-  end
-  
-  function activate_nest(nest, resource_mined)
-    game.print("Nest activate function called")
-    if not nest or not nest.valid then return end
-    local active_nest_name = "active-biter-spawner-generic"
-    -- Change the spawner to the active version
-    for _, resource_table_data in pairs(resource_list) do
+end
+
+function deactivate_nest(nest)
+  if not nest or not nest.valid then return end
+  local inactive_nest_name = "in" .. nest.name
+  local surface = game.surfaces["nauvis"]
+  local inactive_nest = surface.create_entity({
+    name = inactive_nest_name,
+    position = nest.position,
+    force = "enemy"
+  })
+  inactive_nest.health = nest.health
+
+  nest.destroy()
+end
+
+function activate_nest(nest, resource_mined)
+  game.print("Nest activate function called")
+  if not nest or not nest.valid then return end
+  --local active_nest_name = "active-biter-spawner-generic"
+  -- Change the spawner to the active version
+  --[[   for _, resource_table_data in pairs(resource_list) do
       local resource_table = resource_table_data.name
       if resource_table == resource_mined then
         active_nest_name = "active-biter-spawner-" .. resource_mined
         break
       end
-    end
-    game.print("Nest activated at " .. nest.position.x .. ", " .. nest.position.y)
-    local surface = game.surfaces["nauvis"]
-    local active_nest = surface.create_entity({
-      name = active_nest_name,
-      position = nest.position,
-      force = "enemy"
-    })
-  
-    local activate_particles = "ground-explosion"
-  
-    surface.create_entity({
-      name = activate_particles,
-      position = nest.position,
-      force = "enemy"
-    })
-  
-    --set active nest health to inactive_nest
-    active_nest.health = nest.health
-    --adds the activated nest to the active_nests table. will check every to see if it should go dorment.
-    table.insert(storage.active_nests, {
-      entity = active_nest,
-      lastspawn = game.tick,
-      resource = resource_mined,
-    })
-  
-    -- Destroy the inactive nest
-    nest.destroy()
-  
-    --return active_nests
+    end ]]
+  --remove the "in" from the inactive nest name to get the active nest name.
+  local active_nest_name = nest.name:sub(3)
+  game.print("Nest activated at " .. nest.position.x .. ", " .. nest.position.y)
+  local surface = game.surfaces["nauvis"]
+  local active_nest = surface.create_entity({
+    name = active_nest_name,
+    position = nest.position,
+    force = "enemy"
+  })
+
+  local activate_particles = "ground-explosion"
+
+  surface.create_entity({
+    name = activate_particles,
+    position = nest.position,
+    force = "enemy"
+  })
+  if not resource_mined then
+    resource_mined = active_nest_name:sub(22)
   end
+  --set active nest health to inactive_nest
+  active_nest.health = nest.health
+  --adds the activated nest to the active_nests table. will check every to see if it should go dorment.
+  table.insert(storage.active_nests, {
+    entity = active_nest,
+    lastspawn = game.tick,
+    resource = resource_mined,
+  })
+
+  -- Destroy the inactive nest
+  nest.destroy()
+
+  --return active_nests
+end
+
+--checks if a player mines a resource patch.
+script.on_event(defines.events.on_player_mined_entity, function(event)
+  local entity = event.entity
+  local nest_type = "generic"
+  game.print("Entity mined: " .. entity.name)
+  for _, resource_check_data in pairs(resource_list) do
+    local resource_check = resource_check_data.name
+    if resource_check == entity.name then
+      nest_type = entity.name
+      break
+    end
+  end
+  local nests = entity.surface.find_entities_filtered {
+    name = "inactive-biter-spawner-" .. nest_type,
+    position = entity.position,
+    radius = 62
+  }
+  for _, nest in pairs(nests) do
+    if not nest.valid then return end
+    activate_nest(nest, event.entity.name)
+  end
+end)
+
+--checks if a player places a mining drill, pumpjack or turret.
+script.on_event({ defines.events.on_built_entity, defines.events.on_robot_built_entity }, function(event)
+  local entity = event.created_entity or event.entity
+
+  if not (entity and entity.valid) then return end
+
+  local entity_type = entity.type
+  local entity_name = entity.name
+
+  -- Check by entity type
+  if entity_type == "mining-drill" or entity_type == "ammo-turret" or entity_type == "pumpjack" then
+    --game.print("Placed a mining drill or turret: " .. entity_name)
+    local nests = entity.surface.find_entities_filtered {
+      type = "unit-spawner",
+      position = entity.position,
+      radius = 18
+    }
+    if #nests > 0 then
+      game.print("Found nests nearby: " .. #nests)
+      for _, nest in pairs(nests) do
+        if not nest.valid then return end
+        if string.find(nest.name, "inactive") then
+          game.print("Found inactive nest: " .. nest.name)
+
+          activate_nest(nest)
+        end
+      end
+    end
+  end
+end)
--- script/nest_attack.lua
+++ script/nest_attack.lua
@@ -1,33 +1,44 @@
 require "prototypes.biter-data"
 
-local function shoot_boulder(spawner, resource_type)
+local function shoot_boulder(spawner, resource_type, target_position)
     local angle = math.random() * math.pi * 2
     local surface = spawner.surface
+    local target_coordinates
+    if target_position then
+        target_coordinates = target_position
+    else
+        target_coordinates = {
+            spawner.position.x + (math.cos(angle) * math.random(15, 66)),
+            spawner.position.y + (math.sin(angle) * math.random(15, 66))
+        }
+    end
     surface.create_entity({
         name = "boulder-stream-" .. resource_type,
         position = spawner.position,
-        target = {
-            spawner.position.x + (math.cos(angle) * math.random(15, 40)),
-            spawner.position.y + (math.sin(angle) * math.random(15, 40))
-        },
-        speed = 0.5,
+        target = target_coordinates,
+        speed = 0.25,
         source = spawner,
         force = game.forces.enemy,
         --target_type = spawner.surface,
     })
 end
 
-local function shoot_acid_stream(spawner, resource_type)
+local function shoot_acid_stream(spawner, resource_type, target_position)
     local angle = math.random() * math.pi * 2
     local surface = spawner.surface
-    local target_position = {
-        x = spawner.position.x + math.cos(angle) * 20,
-        y = spawner.position.y + math.sin(angle) * 20
-    }
+    local target_coordinates
+    if target_position then
+        target_coordinates = target_position
+    else
+        target_coordinates = {
+            spawner.position.x + (math.cos(angle) * math.random(15, 66)),
+            spawner.position.y + (math.sin(angle) * math.random(15, 66))
+        }
+    end
     surface.create_entity({
         name = "spitter-stream-" .. resource_type,
         position = spawner.position,
-        target_position = target_position,
+        target_position = target_coordinates,
         speed = .3,
         source = spawner,
         force = game.forces.enemy,
@@ -35,20 +46,54 @@ local function shoot_acid_stream(spawner, resource_type)
     })
 end
 
-function shoot_nest_projectile(spawner, resource_type)
+
+local function find_nearby_turrets(spawner)
+    local surface = spawner.surface
+    -- Define the area around the spawner to search for turrets
+    local radius = 35
+    local area = {
+        { spawner.position.x - radius, spawner.position.y - radius },
+        { spawner.position.x + radius, spawner.position.y + radius }
+    }
+    local turrets = surface.find_entities_filtered {
+        area = area,
+        type = "ammo-turret"
+    }
+
+    if #turrets > 0 then
+        return turrets[math.random(#turrets)].position -- Pick one turret at random
+    else
+        return nil
+    end
+end
+
+
+--Checks nest type to determine which projectile to fire, and if it should fire at a turret or not.
+local function projectile_type(spawner, resource_type)
+    local target_position
+    local roll = math.random()
+    if roll < 0.2 then
+        target_position = find_nearby_turrets(spawner)
+    end
+
     if resource_list[resource_type].spawner_data.nest_attack == "boulder" then
-        shoot_boulder(spawner, resource_type)
+        shoot_boulder(spawner, resource_type, target_position)
     elseif resource_list[resource_type].spawner_data.nest_attack == "spitter" then
-        shoot_acid_stream(spawner, resource_type)
+        shoot_acid_stream(spawner, resource_type, target_position)
     elseif resource_list[resource_type].spawner_data.nest_attack == "both" then
         local roll = math.random()
-        if roll < 0.6 then
-            shoot_boulder(spawner, resource_type)
-            shoot_acid_stream(spawner, resource_type)
-        elseif roll > 0.6 and roll < 0.75 then
-            shoot_acid_stream(spawner, resource_type)
-        elseif roll >= 0.75 then
-            shoot_acid_stream(spawner, resource_type)
+        if roll <= 0.5 then
+            shoot_acid_stream(spawner, resource_type, target_position)
+        elseif roll > 0.5 then
+            shoot_acid_stream(spawner, resource_type, target_position)
         end
+    end
+end
+--called every nth tick. random chance to actually fire when called. 
+function shoot_nest_projectile(spawner, resource_type)
+    local roll = math.random()
+    if roll < 0.4 then
+        projectile_type(spawner, resource_type)
+    elseif roll >= 0.4 then
     end
 end
